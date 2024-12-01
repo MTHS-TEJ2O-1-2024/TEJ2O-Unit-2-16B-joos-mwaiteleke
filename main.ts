7c945fb907c0e6fb1b5d9b52f913bb89055f5719
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-12-01 13:33:14 -0500

    Communicates with microbit

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joos
+ * Created on: Dec 1
+ * This program communicates with another microbit if something is too close
 */
 
-basic.showString('Hello, World!')
+basic.showIcon(IconNames.Happy)
+radio.setGroup(1)
+let objectDistance = 0
+
+//getting distance from sonar
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    objectDistance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    if (objectDistance <= 10) {
+        radio.sendString("Too close!")
+    }
+    basic.pause (750)
+})
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
